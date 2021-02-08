870a417fedd44406170a73805356f34b31113b12
Angelica <santosangelicassp@gmail.com>
2021-02-07 23:02:11 -0300

    arrumando erro

--- script.js
+++ script.js
@@ -118,9 +118,9 @@ const DOM = {
 
 const Utils = {
     formatAmount(value) {
-        value = Number(value.replace(/\,\./g, "")) * 100
+        value = value * 100
 
-        return value
+        return Math.round(value)
     },
 
     formatDate(date) {
